7d1b22db71d1535c71f4ad4362eb1a2945e44b73
Huy Nguyễn Văn <huynv1212@gmail.com>
2025-01-23 21:43:47 +0700

    Add files via upload

---
+++ seq_pkg.sv
@@ -0,0 +1,18 @@
+`ifndef GUARD_UART_SEQ_PKG_SV
+`define GUARD_UART_SEQ_PKG_SV
+
+package seq_pkg;
+    import uvm_pkg::*;
+    import uart_pkg::*;
+
+    // Include your file
+          `include "uart_lhs_txrx_sequence.sv"
+          `include "uart_even_sequence.sv"
+          `include "uart_odd_sequence.sv"
+
+endpackage: seq_pkg
+
+`endif
+
+
+        
